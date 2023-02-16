1006c035ece49a0fb794f2d1114e901a0e3ba8e2
wdf332 <wdf332@gmail.com>
2023-02-16 15:55:51 +0800

    edit reserve function

--- controllers/reserve.js
+++ controllers/reserve.js
@@ -1,5 +1,6 @@
 import reserve from '../models/reserve.js'
 import users from '../models/users.js'
+import moment from 'moment'
 
 export const createReserve = async (req, res) => {
   try {
@@ -64,7 +65,7 @@ export const makeReserve = async (req, res) => {
     })
 
     await Promise.all([reservefind.save(), user.save()])
-
+    deleteReserveWithZeroMembers()
     res.status(200).json({ success: true, message: result })
   } catch (error) {
     res.status(500).json({ success: false, message: error.message })
@@ -86,7 +87,7 @@ export const deleteReservation = async (req, res) => {
     const reservations = usersWithReservations.flatMap(user => user.reserve)
     const reservation = reservations.find(r => r._id.equals(req.params.id))
     if (!reservation) {
-      return res.status(404).json({ success: false, message: 'Reservation not found' })
+      return res.status(404).json({ success: false, message: '找不到' })
     }
     // Remove the reservation from the user's reserve array
     const user = await users.findOne({ reserve: reservation })
@@ -97,58 +98,48 @@ export const deleteReservation = async (req, res) => {
     // Delete the reservation from the database
     await reservation.remove()
 
-    res.status(200).json({ success: true, message: 'Reservation deleted' })
+    res.status(200).json({ success: true, message: '預約已刪除' })
   } catch (error) {
     res.status(500).json({ success: false, message: error.message })
   }
 }
 
-// export const createOrder = async (req, res) => {
-//   try {
-//     // 檢查購物車是不是空的
-//     if (req.user.cart.length === 0) {
-//       res.status(400).json({ success: false, message: '購物車是空的' })
-//       return
-//     }
-//     // 檢查是否有下架商品
-//     let result = await users.findById(req.user._id, 'cart').populate('cart.p_id')
-//     const canCheckout = result.cart.every(cart => {
-//       return cart.p_id.sell
-//     })
-//     if (!canCheckout) {
-//       res.status(400).json({ success: false, message: '包含下架商品' })
-//       return
-//     }
-//     // 建立訂單
-//     result = await orders.create({ u_id: req.user._id, products: req.user.cart })
-//     // 清空購物車
-//     req.user.cart = []
-//     await req.user.save()
-//     res.status(200).json({ success: true, message: '' })
-//   } catch (error) {
-//     if (error.name === 'ValidationError') {
-//       res.status(400).json({ success: false, message: error.errors[Object.keys(error.errors)[0]].message })
-//     } else {
-//       res.status(500).json({ success: false, message: '未知錯誤' })
-//     }
-//   }
-// }
+// ---------------------------------------------
+export const createReservationsForWeek = async (req, res) => {
+  try {
+    const { startDate, endDate, time, member } = req.body
 
-// export const getMyOrders = async (req, res) => {
-//   try {
-//     const result = await orders.find({ u_id: req.user._id }).populate('products.p_id')
-//     res.status(200).json({ success: true, message: '', result })
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: '未知錯誤' })
-//   }
-// }
+    // Convert start and end dates to moment objects and set start time to 9am
+    const start = moment(startDate).startOf('day').add(9, 'hours')
+    const end = moment(endDate).startOf('day').add(9, 'hours')
 
-// export const getAllOrders = async (req, res) => {
-//   try {
-//     // .populate(關聯資料路徑, 取的欄位)
-//     const result = await orders.find().populate('products.p_id').populate('u_id', 'account')
-//     res.status(200).json({ success: true, message: '', result })
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: '未知錯誤' })
-//   }
-// }
+    // Create reservations for each day
+    const reservations = []
+    let current = start
+    while (current.isSameOrBefore(end)) {
+      // Only create reservations for days that the business is open
+      if (current.isoWeekday() <= 7) {
+        const newReservation = await reserve.create({
+          date: current.format('YYYY/MM/DD'),
+          time,
+          member
+        })
+        reservations.push(newReservation)
+      }
+      current = current.add(1, 'day')
+    }
+
+    res.status(200).json({ success: true, message: '一周預約建立成功', reservations })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+}
+
+const deleteReserveWithZeroMembers = async () => {
+  try {
+    await reserve.deleteOne({ member: 0 })
+    console.log('已刪除人數為0')
+  } catch (error) {
+    console.error(error)
+  }
+}
--- routes/reserve.js
+++ routes/reserve.js
@@ -3,16 +3,26 @@ import content from '../middleware/content.js'
 import admin from '../middleware/admin.js'
 // import upload from '../middleware/upload.js'
 import * as auth from '../middleware/auth.js'
-import { createReserve, getReservelimit, deleteReserve, makeReserve, findAllUsersReserves, deleteReservation } from '../controllers/reserve.js'
+import {
+  createReserve,
+  getReservelimit,
+  deleteReserve,
+  makeReserve,
+  findAllUsersReserves,
+  deleteReservation,
+  createReservationsForWeek
+} from '../controllers/reserve.js'
 
 const router = Router()
 
 router.post('/', content('application/json'), auth.jwt, admin, createReserve)
+router.post('/week', content('application/json'), auth.jwt, admin, createReservationsForWeek)
 router.get('/', getReservelimit)
 router.delete('/:id', auth.jwt, deleteReserve)
 router.post('/make', auth.jwt, makeReserve)
 router.get('/all', auth.jwt, findAllUsersReserves)
 router.delete('/delete/:id', auth.jwt, deleteReservation)
+
 // router.get('/all', jwt, admin, getAllProducts)
 // router.get('/:id', getProduct)
 // router.patch('/:id', content('multipart/form-data'), jwt, admin, upload, editProduct)
