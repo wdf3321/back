cf35a4c728bba1c37142ea408098cfe7d6077e69
wdf <wdf332@gmail.com>
2023-03-09 00:23:45 +0800

    edit back

--- controllers/articles.js
+++ controllers/articles.js
@@ -24,6 +24,22 @@ export const getAllArticles = async (req, res) => {
     res.status(500).json({ success: false, message: '未知錯誤' })
   }
 }
+export const getOneArticle = async (req, res) => {
+  try {
+    const result = await articles.findById(req.params.id)
+    if (!result) {
+      res.status(404).json({ success: false, message: '找不到' })
+    } else {
+      res.status(200).json({ success: true, message: '', result })
+    }
+  } catch (error) {
+    if (error.name === 'CastError') {
+      res.status(404).json({ success: false, message: '找不到' })
+    } else {
+      res.status(500).json({ success: false, message: '未知錯誤' })
+    }
+  }
+}
 
 export const deleteArticles = async (req, res) => {
   try {
--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import { jwt } from '../middleware/auth.js'
 import admin from '../middleware/admin.js'
-import { createArticles, getAllArticles, deleteArticles, deleteSelectArticles } from '../controllers/articles.js'
+import { createArticles, getAllArticles, deleteArticles, deleteSelectArticles, getOneArticle } from '../controllers/articles.js'
 //, deleteArticles
 
 const router = Router()
 
 router.post('/create', jwt, admin, createArticles)
 router.get('/', getAllArticles)
+router.get('/:id', getOneArticle)
 router.delete('/delete/', jwt, admin, deleteArticles)
 router.delete('/delete/:id', jwt, admin, deleteSelectArticles)
 
